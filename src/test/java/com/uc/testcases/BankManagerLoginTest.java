73e05d0a5d7a4b57af9c6fbc1ae2e008b5612262
uc <info@uc>
2018-07-07 09:29:13 -0500

    Update TestNG report HTML formatting with Repporter.log

--- src/test/java/com/uc/testcases/BankManagerLoginTest.java
+++ src/test/java/com/uc/testcases/BankManagerLoginTest.java
@@ -2,6 +2,7 @@ package com.uc.testcases;
 
 import org.openqa.selenium.By;
 import org.testng.Assert;
+import org.testng.Reporter;
 import org.testng.annotations.Test;
 
 import com.uc.base.TestBase;
@@ -12,6 +13,13 @@ public class BankManagerLoginTest extends TestBase {
 	@Test
 	public void loginAsBankManager() throws InterruptedException {
 		//Arrange
+
+		
+		// for hyperlinks in testng reports
+		System.setProperty("org.uncommons.reportng.escape-output","false");
+		
+		
+		
 		log.debug("Inside Login Test");
 		
 		//Act
@@ -20,15 +28,12 @@ public class BankManagerLoginTest extends TestBase {
 		
 		//Assert
 		Assert.assertTrue(isElementPresent(By.cssSelector(OR.getProperty("addCustBtn"))),"Login not successful");
-		log.debug("Inside succesfully executed");
+		log.debug("Login succesfully executed");
+		Reporter.log("Login succesfully executed");
+		Reporter.log("<a target=\"_blank\" href=\"C:\\a\\d\\capture\\2018.07.07.021102.cap.jpg\">Screenshot</a><br/>");
+		Reporter.log("<a href=\"C:\\a\\d\\capture\\2018.07.07.021102.cap.jpg\"><img height=\"50%\" width=\"50%\" src=\"C:\\a\\d\\capture\\2018.07.07.021102.cap.jpg\"/></a>");
 	}
 
-	
-	
-	
-	
-	
-	
 	
 	public boolean isElementPresent(By by) {
 		
