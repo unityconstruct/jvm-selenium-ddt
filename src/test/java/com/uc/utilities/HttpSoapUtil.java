0d2cb7f9965f360ee646ac6d6da78ad7cfc9a12e
uc <info@uc>
2018-07-20 04:24:11 -0500

    More updates to SOAP/XML handling

--- src/test/java/com/uc/utilities/HttpSoapUtil.java
+++ src/test/java/com/uc/utilities/HttpSoapUtil.java
@@ -1,11 +1,19 @@
 package com.uc.utilities;
+import java.io.File;
 import java.util.ArrayList;
 import java.util.Iterator;
 
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.soap.*;
+import javax.xml.xpath.XPath;
+import javax.xml.xpath.XPathConstants;
+import javax.xml.xpath.XPathExpression;
+import javax.xml.xpath.XPathFactory;
 
 import org.apache.xmlbeans.XmlFactoryHook;
 //import org.w3c.dom.NodeList;
+import org.w3c.dom.Document;
 
 
 public class HttpSoapUtil {
@@ -184,5 +192,83 @@ public class HttpSoapUtil {
 	        return soapMessage;
 	    }
 
+//
+//	    
+//	    
+	    // REVIEW & BREAKDOWN
+	    private static void parseSoapXml() {
+	    	//https://stackoverflow.com/questions/44152924/parsing-a-soap-response-using-xpath-java#44162280
+	    	try 
+	        {
+	            DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+	            dbf.setNamespaceAware(true);
+	            DocumentBuilder db = dbf.newDocumentBuilder();
+
+	            Document doc = db.parse( new File( "soapResponse.xml" ) );
+
+	            XPathFactory xPathFactory = XPathFactory.newInstance();
+	            XPath xpath = xPathFactory.newXPath();
+	            javax.xml.namespace.NamespaceContext ns = new javax.xml.namespace.NamespaceContext()
+	            {
+
+	                @Override
+	                public String getNamespaceURI(String prefix) 
+	                {
+	                    if ( "soap".equals( prefix ) )
+	                    {
+	                        return "http://schemas.xmlsoap.org/soap/envelope/";
+	                    }
+	                    else if ( "xsi".equals( prefix ) )
+	                    {
+	                        return "http://www.w3.org/2001/XMLSchema-instance";
+	                    }
+	                    else if ( "xsd".equals( prefix ) )
+	                    {
+	                        return "http://www.w3.org/2001/XMLSchema";
+	                    }
+	                    else if ( "xml".equals( prefix ) )
+	                    {
+	                        return javax.xml.XMLConstants.XML_NS_URI;
+	                    }
+	                    else if ( "parentns".equals( prefix ) )
+	                    {
+	                        return "urn:JadeWebServices/NetsuiteCustomer/";
+	                    }
+
+	                    return javax.xml.XMLConstants.NULL_NS_URI;
+	               }
+
+	               @Override
+	               public String getPrefix(String namespaceURI) 
+	               {
+	                  return null;
+	               }
+
+	                @Override
+	                public Iterator<?> getPrefixes(String namespaceURI) 
+	                {
+	                    return null;
+	                }
+
+	            };
+
+
+	             xpath.setNamespaceContext(ns);
+	             XPathExpression expr = xpath.compile( "/soap:Envelope/soap:Body/parentns:addParentResponse/parentns:addParentResult/text()" );
+
+
+	             Object exprEval = expr.evaluate( doc, XPathConstants.STRING );
+	             if ( exprEval != null )
+	             {
+	                System.out.println( "The text of addParentResult is : " + exprEval );
+	             }
+	        }
+	        catch ( Exception e )
+	        {
+	            e.printStackTrace();
+	        }
+	     }
+	    
+	    
 	
 }
